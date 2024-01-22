f0196e9670cbeefc8cfdcc4cd4d896a884e19327
BoRoDaTeNkiY <polpoltrop228@gmail.com>
2024-01-22 19:53:47 +0200

    buffer

--- WeatherAppC/WeatherAppC.cpp
+++ WeatherAppC/WeatherAppC.cpp
@@ -8,12 +8,15 @@ size_t write_chunk(void* data, size_t size, size_t nmemb, void* userdata);
 int main() {
 	CURL* curl;
 	CURLcode response;
+
+	char buffer[256];
+	sprintf_s(buffer, sizeof(buffer), "https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=94.04&appid=%s", WEATHER_API_KEY);
+
 	curl = curl_easy_init();
 	curl_easy_setopt(
 		curl, 
 		CURLOPT_URL, 
-		"https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=%s", 
-		WEATHER_API_KEY
+		buffer
 	);
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_chunk);
 	response = curl_easy_perform(curl);
@@ -23,12 +26,10 @@ int main() {
 }
 
 size_t write_chunk(void* data, size_t size, size_t nmemb, void* userdata) {
-	printf("\n\nBEGIN CHUNK\n\n");
-
+	printf("\n\n");
 	for (size_t i = 0; i < nmemb; i++) {
 		printf("%c", ((char*)data)[i]);
 	}
-	printf("\n\nEND CHUNK\n\n");
-
+	printf("\n\n");
 	return nmemb;
 }
